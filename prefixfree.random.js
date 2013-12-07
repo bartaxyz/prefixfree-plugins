c7566a0d3ac2c2760dbe63a8a3c3a389dd92fb71
PageOnline <sims.Barton.X@gmail.com>
2013-12-07 09:48:29 +0100

    prefixfree.random.js update

--- prefixfree.random.js
+++ prefixfree.random.js
@@ -4,14 +4,24 @@
 	}
 
 	StyleFix.register(function(css) {
-		return css.replace(/random\((\d+)\s*-\s*(\d+)\)/gi, function(exp, minNum, maxNum) {
-			var max = parseInt(maxNum, 10),
+		return css.replace(/random\(([\w\d]+)\s*[-,]\s*([\w\d]+)\)/gi, function(exp, minNum, maxNum) {
+			var max, min, abc;
+			if(/[a-fA-F]/.test(minNum) || /[a-fA-F]/.test(maxNum)) {
+				max = parseInt(maxNum, 16);
+				min = parseInt(minNum, 16);
+				abc = 16;
+			} else {
+				max = parseInt(maxNum, 10);
 				min = parseInt(minNum, 10);
-			if(max < min) {
-				max = parseInt(minNum, 10);
-				min = parseInt(maxNum, 10);
+				abc = 10;
 			}
-			return Math.floor(Math.random() * (max - min) + min);
+			if(max < min) {
+				max = parseInt(minNum, abc);
+				min = parseInt(maxNum, abc);
+			}
+			var result = Math.floor(Math.random() * (max - min) + min);
+			if(abc == 16) result = result.toString(16);
+			return result;
 		});
 	});
 })();
